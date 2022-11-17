608c4b85b6944aa5acb6ba317a466e42d473c028
Evgeniy Shesterikov <67272160+evgen4ikrus@users.noreply.github.com>
2022-11-17 10:11:12 +0300

    add beautiful cart display

--- format_message.py
+++ format_message.py
@@ -5,9 +5,15 @@ def create_product_description(product):
     return text
 
 
-# Доделать получение цены и передать красивое содержимое корзины
 def create_cart_description(cart_items):
-    description = ''
+    cart_description = ''
+    total_price = 0
     for item in cart_items:
-        description += f'{item.get("name")}\n{item.get("description")}\n\n'
-    return description
+        one_for_price = item.get('unit_price').get('amount') * 0.01
+        price = one_for_price * item.get("quantity")
+        total_price += price
+        cart_description += f'''{item.get("name")}
+{item.get("description")}
+{item.get("quantity")}шт. за {price}$ ({one_for_price}$ за шт.)\n\n'''
+    cart_description += f'Общая цена: {total_price}$'
+    return cart_description
